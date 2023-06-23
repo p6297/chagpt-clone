6c5f6c2b5000986aa41b7f167cb029650dd848a4
Partha Jyoti Pandey <parthajyoti.pandey45@gmail.com>
2023-06-23 19:39:49 +0530

    minimum ui setup

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const App = () => {
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>hi usbsjsjs jskskks</li>
+        </ul>
+        <nav>
+          <p>Made By Partha</p>
+          </nav>
+
+      </section>
+
+      <section className="main">
+        <h1>GPT-PARTHA</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➤</div>
+            
+          </div>
+          <p className="info">Every clone begins with limited features,but it is through learning and growth that they expand their capabilities and reach their true potential.</p>
+
+        </div>
+
+      </section>
+     
+    </div>
+  );
+}
+
+export default App;
